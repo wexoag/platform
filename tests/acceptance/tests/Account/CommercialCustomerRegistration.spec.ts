461f721791c919e5b01210ae7d726cb2340c20ea
Yusuf Tür <y.tuer@shopware.com>
2024-12-05 11:33:20 +0000

    NEXT-39285 - Add commercial customer registration test

---
+++ tests/acceptance/tests/Account/CommercialCustomerRegistration.spec.ts
@@ -0,0 +1,22 @@
+import { test } from '@fixtures/AcceptanceTest';
+
+test('As a new customer, I must be able to register as a commercial customer in the Storefront.', { tag: '@Registration' }, async ({
+    ShopCustomer,
+    ShopAdmin,
+    StorefrontAccountLogin,
+    StorefrontAccount,
+    IdProvider,
+    Register,
+    SetSystemConfigValues,
+}) => {
+    const uuid = IdProvider.getIdPair().uuid;
+    const customer = { email: uuid + '@test.com', vatRegNo: uuid + '-VatId' };
+    const setAccountType = 'core.loginRegistration.showAccountTypeSelection';
+    await ShopAdmin.attemptsTo(SetSystemConfigValues({ [setAccountType]: true }, { [setAccountType]: false }));
+
+    await ShopCustomer.goesTo(StorefrontAccountLogin.url());
+    await StorefrontAccountLogin.accountTypeSelect.selectOption('Commercial');
+    await ShopCustomer.attemptsTo(Register(customer, true));
+    await ShopCustomer.expects(StorefrontAccount.page.getByText(customer.email, { exact: true })).toBeVisible();
+    await ShopCustomer.expects(StorefrontAccount.page.getByText(customer.vatRegNo)).toBeVisible();
+});

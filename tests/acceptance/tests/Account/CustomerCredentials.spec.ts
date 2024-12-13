488a3b70610870e56de179f91483ebb843876cfa
Orkun Cavli <o.cavli@shopware.com>
2024-12-13 13:16:51 +0100

    NEXT-39784 - fix typo and comment lines

--- tests/acceptance/tests/Account/CustomerCredentials.spec.ts
+++ tests/acceptance/tests/Account/CustomerCredentials.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@fixtures/AcceptanceTest';
 
-test('As a new customer, I must be able to change email and password via account.', { tag: '@Account' }, async ({
+test('As a customer, I must be able to change email via account.', { tag: '@Account' }, async ({
     ShopCustomer,
     StorefrontAccountLogin,
     StorefrontAccount,
@@ -13,27 +13,44 @@ test('As a new customer, I must be able to change email and password via account
     const customer = { email: IdProvider.getIdPair().uuid + '@test.com' , password: IdProvider.getIdPair().uuid };
     const changedEmail = { email: IdProvider.getIdPair().uuid + '@test.com' };
 
-
-    //customer login
     await ShopCustomer.goesTo(StorefrontAccountLogin.url());
     await ShopCustomer.attemptsTo(Register(customer));
     await ShopCustomer.expects(StorefrontAccount.page.getByText(customer.email, { exact: true })).toBeVisible();
-
-    //open profile
     await ShopCustomer.goesTo(StorefrontAccountProfile.url());
     await StorefrontAccountProfile.changeEmailButton.click();
     await ShopCustomer.expects(StorefrontAccountProfile.emailAddressInput).toBeVisible();
     await StorefrontAccountProfile.emailAddressInput.fill(changedEmail.email);
     await StorefrontAccountProfile.emailAddressConfirmInput.fill(changedEmail.email);
     await StorefrontAccountProfile.emailConfirmPasswordInput.fill(customer.password);
-    // eslint-disable-next-line no-console
-    await console.log(customer.password);
     await StorefrontAccountProfile.saveEmailAddressButton.click();
+    //await ShopCustomer.expects(StorefrontAccountProfile.updateMessage).toBeVisible(); //Your email address has been updated.
+    //await ShopCustomer.expects(StorefrontAccountProfile.loginDataEmail).toContain(changedEmail.email);
+});
 
 
+test('As a customer, I must be able to change password via account.', { tag: '@Account' }, async ({
+    ShopCustomer,
+    StorefrontAccountLogin,
+    StorefrontAccount,
+    IdProvider,
+    Register,
+    StorefrontAccountProfile,
 
-    //change email
-    //change password
-    //verify change
+}) => {
 
+    const customer = { email: IdProvider.getIdPair().uuid + '@test.com' , password: IdProvider.getIdPair().uuid };
+    const changedPassword = { password: IdProvider.getIdPair().uuid };
+
+    await ShopCustomer.goesTo(StorefrontAccountLogin.url());
+    await ShopCustomer.attemptsTo(Register(customer));
+    await ShopCustomer.expects(StorefrontAccount.page.getByText(customer.email, { exact: true })).toBeVisible();
+    await ShopCustomer.goesTo(StorefrontAccountProfile.url());
+    await StorefrontAccountProfile.changePasswordButton.click();
+    await ShopCustomer.expects(StorefrontAccountProfile.newPasswordInput).toBeVisible();
+    await StorefrontAccountProfile.newPasswordInput.fill(changedPassword.password);
+    await StorefrontAccountProfile.newPasswordConfirmInput.fill(changedPassword.password);
+    await StorefrontAccountProfile.emailConfirmPasswordInput.fill(customer.password);
+    await StorefrontAccountProfile.saveNewPasswordButton.click();
+    //await ShopCustomer.expects(StorefrontAccountProfile.updateMessage).toBeVisible(); //Your password has been updated.
+    //await ShopCustomer.expects(StorefrontAccountProfile.loginDataEmail).toContain(changedEmail.email);
 });
